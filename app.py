84f70dd8705a185d25120773426a4576bf04d74e
Daniel <89271498+Daniel-Valadares@users.noreply.github.com>
2024-12-01 06:21:40 +0000

    asasasdd

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ load_dotenv()
 os.getenv("AIzaSyB8k3CqdZngSYKdcZH1RyBbOIdCYEvHTjg")
 genai.configure(api_key=os.getenv("AIzaSyB8k3CqdZngSYKdcZH1RyBbOIdCYEvHTjg"))
 
-def update_values(list, documents):
+def update_values(list,documents):
     if documents:
         for document in documents:
             list.append(document.name)
@@ -45,21 +45,20 @@ def process_pdf(documents):
 
     return important_info.strip()
 
-
-def process_chunks(text, chunk_size, chunk_overlap):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+def process_chunks(text,chunk_size,chunk_overlap):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size,chunk_overlap=chunk_overlap)
     chunks = text_splitter.split_text(text)
     return chunks
 
-
 def embed_text(text_chunks):
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    vectors = FAISS.from_texts(text_chunks, embeddings)
+    vectors=FAISS.from_texts(text_chunks,embeddings)
     vectors.save_local("faiss_index")
 
 
 def prompt_template():
-    base_template = """
+    
+    base_template=  """
     Por favor, responda à pergunta de forma detalhada e completa, utilizando as informações do contexto fornecido. Caso não seja possível responder com base no contexto, diga "Não consegui formular uma resposta :(".
     Contexto: 
     {context}
@@ -77,57 +76,85 @@ def prompt_template():
 
     """
 
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.5)
+    model=ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.5)
 
-    prompt = PromptTemplate(template=base_template, input_variables=["context", "question"])
-
-    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    prompt = PromptTemplate(template = base_template, input_variables = ["context", "question"])
+    
+    chain=load_qa_chain(model,chain_type="stuff",prompt=prompt)
 
     return chain
 
 
 def answer_question(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
 
-    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
-
+    new_db = FAISS.load_local("faiss_index", embeddings,allow_dangerous_deserialization=True)
+    
     docs = new_db.similarity_search(user_question)
 
-    chain = prompt_template()
+    chain=prompt_template()
 
     response = chain(
-        {"input_documents": docs, "question": user_question},
-        return_only_outputs=True)
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
 
-    st.write("Resposta:", response["output_text"])
-
-
+    
+    #print(response)
+    st.write("Reply: ", response["output_text"])
+    
 def clear_cache():
     streamlit_js_eval(js_expressions="parent.window.location.reload()")
 
 
+    
+
 def main():
-    PAGE_CONFIG = {"page_title": "Assistente Inteligente", "layout": "centered"}
+
+    PAGE_CONFIG = {"page_title":"Assistente LLM",  
+               "layout":"centered", 
+               "initial_sidebar_state":"collapsed"}
+
     st.set_page_config(**PAGE_CONFIG)
 
-    st.title('Interprete PDFs com Inteligência Artificial')
-    st.subheader("Faça perguntas baseadas nos documentos carregados!")
+
+
+    st.title('Assistente Conversacional baseado em LLM')
+    st.header("Envie seus arquivos PDF e faça perguntas!")
 
     if 'chunk_size' not in st.session_state:
         st.session_state.chunk_size = 500
+    
+    with st.sidebar:
 
-    documents = st.file_uploader("Carregue seus documentos PDF aqui:", type=['pdf'], accept_multiple_files=True)
+        st.markdown("## Tecnologias Utilizadas")
+        st.markdown("1. **Modelo LLM:** [Gemini-Pro (Google API)](https://console.cloud.google.com/marketplace/product/google/gemini-pro)")
+        st.markdown("2. **Embeddings:** [Google - LangChain](https://console.cloud.google.com/marketplace/product/google/generativeai)")
+        st.markdown("3. **Indexação de Texto:** [Meta - FAISS](https://ai.meta.com/tools/faiss/)")
+        st.markdown("4. **Frontend:** [Streamlit](https://streamlit.io/)")
 
+        
+
+        if st.button("Limpar Sessão"):
+           clear_cache()
+
+        st.markdown("*As respostas do Gemini podem variar de acordo com a disponibilidade da API, caso a resposta não tenha sido satisfatória ou o assistente não tenha respondido, tente reformular a pergunta (perguntas mais específicas geram resultados melhores!).*")
+        
+
+    documents=st.file_uploader("Faça upload dos seus documentos", type=['pdf'], accept_multiple_files=True)
+    
     file_names = ['Todos']
 
-    st.button("Enviar", on_click=update_values(file_names, documents))
 
-    selected_files = st.multiselect("Escolha os arquivos para análise:", file_names, placeholder="Selecione os documentos")
+    st.button("Enviar",on_click=update_values(file_names,documents))
+        
 
-    selected_documents = []
+    selected_files = st.multiselect("Selecione os arquivos a serem utilizados como contexto:", file_names,placeholder="Selecione os arquivos")
+
+
+    selected_documents=[]
 
     if 'Todos' in selected_files:
-        selected_documents = documents
+        selected_documents=documents
     else:
         for document in documents:
             if document.name in selected_files:
@@ -136,14 +163,17 @@ def main():
     chunk_size = len(selected_documents) * 2000
     chunk_overlap = chunk_size * 0.25
 
-    user_input = st.text_input("Digite sua pergunta aqui:")
-    if st.button("Responder"):
+    user_input = st.text_input("Faça uma Pergunta:")
+    if st.button("Fazer Pergunta"):
         with st.spinner("Processando..."):
-            raw_text = process_pdf(selected_documents)
-            text_chunks = process_chunks(raw_text, chunk_size, chunk_overlap)
+            raw_text=process_pdf(selected_documents)
+            text_chunks=process_chunks(raw_text,chunk_size,chunk_overlap)
             embed_text(text_chunks)
         answer_question(user_input)
-        st.success("Processamento concluído!")
+        st.success("Processamento Concluído!")
+        
+        
+
 
 
 if __name__ == '__main__':
